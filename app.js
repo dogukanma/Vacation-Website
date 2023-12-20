e61f2c634e30104f268ebcfbe299de3446ea7ce3
dogukanma <blanksh364@gmail.com>
2023-12-20 20:48:04 +0300

    +selected_house.html, images

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let heartArr = document.getElementsByClassName("fa-heart");
   let divListingArr = document.getElementsByClassName("div-listing");
+  let imageArr = document.querySelectorAll(".selected-small-images img");
+  let mainSelectedImg = document.querySelector(".div-selected-image > img")
   for (let i = 0; i < heartArr.length; i++) {
     const heartIcon = heartArr[i];
     heartIcon.addEventListener("click", (e) => {
@@ -22,6 +24,26 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   }
 
+  //------ Image expands on hover (Will be added later on) ------//
 
+  // for(let i = 0; i < imageArr.length; i++){
+  //   const focusedImage = imageArr[i];
+  //   focusedImage.addEventListener("mouseover", () => {
+  //     if(focusedImage.style.maxWidth === "25%"){
+  //       focusedImage.style.maxWidth = "30%"
+  //     } else{
+  //       focusedImage.style.maxWidth = "25%"
+  //     }
+  //   })
+  // }
+
+  // Change the selected image on selected_house.html
+  for(let i = 0; i < imageArr.length; i++){
+    const focusedImage = imageArr[i];
+    focusedImage.addEventListener("click", () => {
+      mainSelectedImg.setAttribute("src", focusedImage.getAttribute("src"));
+      // alert(mainSelectedImg.offsetHeight);
+    })
+  }
   
 });
