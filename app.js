6cc5a8f78d6b89835ee6f88d892ecfeacee0264d
dogukanma <blanksh364@gmail.com>
2023-12-27 15:56:00 +0300

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let heartArr = document.getElementsByClassName("fa-heart");
   let divListingArr = document.getElementsByClassName("div-listing");
   let imageArr = document.querySelectorAll(".selected-small-images img");
-  let mainSelectedImg = document.querySelector(".div-selected-image > img")
+  let mainSelectedImg = document.querySelector(".main-image");
   for (let i = 0; i < heartArr.length; i++) {
     const heartIcon = heartArr[i];
     heartIcon.addEventListener("click", (e) => {
@@ -24,25 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
     })
   }
 
-  //------ Image expands on hover (Will be added later on) ------//
-
-  // for(let i = 0; i < imageArr.length; i++){
-  //   const focusedImage = imageArr[i];
-  //   focusedImage.addEventListener("mouseover", () => {
-  //     if(focusedImage.style.maxWidth === "25%"){
-  //       focusedImage.style.maxWidth = "30%"
-  //     } else{
-  //       focusedImage.style.maxWidth = "25%"
-  //     }
-  //   })
-  // }
-
   // Change the selected image on selected_house.html
   for(let i = 0; i < imageArr.length; i++){
     const focusedImage = imageArr[i];
     focusedImage.addEventListener("click", () => {
       mainSelectedImg.setAttribute("src", focusedImage.getAttribute("src"));
-      // alert(mainSelectedImg.offsetHeight);
     })
   }
   
